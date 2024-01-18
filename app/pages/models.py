24bb63a5b54296aa941db52234cb442e18811416
marisa mari <goemon758@gmail.com>
2024-01-18 16:49:06 +0900

    test blog

--- app/pages/models.py
+++ app/pages/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField('category name', max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
+class Tag(models.Model):
+    name = models.CharField('tag name', max_length=50)
+
+    def __str__(self):
+        return self.name
+
+
+class Blog(models.Model):
+    title = models.CharField('title', max_length=100)
+    text = models.TextField('text')
+    category = models.ForeignKey(Category, verbose_name='category', on_delete=models.PROTECT)
+    tag = models.ManyToManyField(Tag, verbose_name='tag')
+    relation = models.ManyToManyField('self', verbose_name='relation', blank=True, null=True)
+    created_at = models.DateTimeField('created date', auto_now_add=True)
+    updated_at = models.DateTimeField('updated date', auto_now=True)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'blog'
+        verbose_name_plural = 'blogs'
--- app/pages/urls.py
+++ app/pages/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path
+from .views import IndexView, DetailView, CategoryView, TagView
 
-from .views import HomePageView
+
+app_name = "pages"
 
 urlpatterns = [
-    path("", HomePageView.as_view(), name="home"),
+    # path('', views.index, name='index'),
+    path("", IndexView.as_view(), name="index"),
+    path('category/<str:category>/', CategoryView.as_view(), name='category'),
+    path('tag/<str:tag>/', TagView.as_view(), name='tag'),
+    path("detail/<int:pk>/", DetailView.as_view(), name="detail"),
 ]
